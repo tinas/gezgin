98003742c90fb22a79bff1d4a9c4225c67d4bdfd
tinas <ahmettinastepe0@gmail.com>
2020-12-24 23:53:30 +0300

    Move under models
    Model files have been moved under the models folder.

--- station.js
+++
@@ -1,9 +0,0 @@
-class Station {
-  constructor(name, location, stationType) {
-    this.name = name
-    this.location = location
-    this.stationType = stationType
-  }
-}
-
-module.exports = Station
