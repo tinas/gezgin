50018d986ad2a52db8537d53d7ec75b8a03cc0ec
tinas <ahmettinastepe0@gmail.com>
2020-12-25 00:09:13 +0300

    Rebuild based on file-based database

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-const colors = require('colors')
+const {passengerDatabase, stationDatabase, scooterDatabase} = require('./database')
+const {printCurrentBooking} = require('./lib/print-booking-history')
+const {printStations} = require('./lib/print-station')
 
-const Passenger = require('./passenger')
-const Station = require('./station')
-const Bike = require('./bike')
-const { StationType } = require('./enums')
+const mehmet = passengerDatabase.findByName('Mehmet')
+const scooter = scooterDatabase.findByBrand('Razor E300')
 
-const stations = []
+mehmet.book(scooter)
+passengerDatabase.update(mehmet)
 
-function printStations() {
-  stations.forEach((station) => {
-    console.log(
-      `${colors.cyan(station.name)} is a ${colors.magenta(
-        station.stationType
-      )} station`
-    )
-  })
-}
+printCurrentBooking(mehmet)
 
-const kadikoyBikeStation = new Station('Kadıköy', [0, 0], StationType.BIKE)
-const tuzlaBikeStation = new Station('Tuzla', [2, 0], StationType.BIKE)
-const pendikScooterStation = new Station('Pendik', [2, 4], StationType.SCOOTER)
-
-stations.push(kadikoyBikeStation)
-stations.push(tuzlaBikeStation)
-stations.push(pendikScooterStation)
-
-const ahmet = new Passenger('Ahmet Tinastepe', '505110', 'ahmttnstpe@gmail.com')
-const bike = new Bike(0.1, 'Giant', kadikoyBikeStation)
-
-ahmet.book(bike)
-ahmet.printCurrentBooking()
 console.log('------------')
 
-ahmet.finishedBooking(tuzlaBikeStation)
-bike.updateLastStation(tuzlaBikeStation)
+const stations = stationDatabase.load()
 
-ahmet.printBookingHistory()
-console.log('------------')
-printStations()
+printStations(stations)
