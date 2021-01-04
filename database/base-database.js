e81bc91d08532c000c3977987d7a1c1a8daee0bf
tinas <ahmettinastepe0@gmail.com>
2021-01-04 16:54:00 +0300

    BaseDatabase must be asynchronous

--- database/base-database.js
+++ database/base-database.js
@@ -8,46 +8,51 @@ class BaseDatabase {
   }
 
   save(objects) {
-    fs.writeFileSync(`${__dirname}/${this.fileName}.json`, flatted.stringify(objects, null, 2))
+    return new Promise((resolve, reject) => {
+      fs.writeFile(`${__dirname}/${this.fileName}.json`, flatted.stringify(objects, null, 2), err => {
+        if (err) return reject(err)
+        resolve()
+      })
+    })
   }
 
   load() {
-    const file = fs.readFileSync(`${__dirname}/${this.fileName}.json`, 'utf8')
-    const objects = flatted.parse(file)
+    return new Promise((resolve, reject) => {
+      fs.readFile(`${__dirname}/${this.fileName}.json`, 'utf8', (err, file) => {
+        if (err) return reject(err)
 
-    return objects.map(this.model.create)
+        const objects = flatted.parse(file)
+        resolve(objects.map(this.model.create))
+      })
+    })
   }
 
-  insert(object) {
-    const objects = this.load()
-
-    this.save(objects.concat(object))
+  async insert(object) {
+    const objects = await this.load()
+    return this.save(objects.concat(object))
   }
 
-  remove(index) {
-    const objects = this.load()
-
-    objects.splice(index, 1)
-    this.save(objects)
+  async remove(index) {
+    const objects = await this.load()
+    return this.save(objects.splice(index, 1))
   }
 
-  update(object) {
-    const objects = this.load()
-
+  async update(object) {
+    const objects = await this.load()
     const index = objects.findIndex(o => o.id == object.id)
 
     if (index == -1) throw new Error(`Cannot find ${this.model.name} instance with id ${object.id}`)
 
     objects.splice(index, 1, object)
-    this.save(objects)
+    return this.save(objects)
   }
 
-  find(id) {
-    return this.load().find(o => o.id == id)
+  async find(id) {
+    return (await this.load()).find(o => o.id == id)
   }
 
-  findBy(property, value) {
-    return this.load().find(o => o[property] == value)
+  async findBy(property, value) {
+    return (await this.load()).find(o => o[property] == value)
   }
 }
 
