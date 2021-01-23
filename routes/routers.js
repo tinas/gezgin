143031d8698d63d5db9cec4692b320f8c164fab9
tinas <ahmettinastepe0@gmail.com>
2021-01-23 22:03:44 +0300

    Add routers file

---
+++ routes/routers.js
@@ -0,0 +1,17 @@
+const indexRouter = require('./index')
+const passengersRouter = require('./passengers')
+const stationsRouter = require('./stations')
+const parkingUnitsRouter = require('./parking-units')
+const bikesRouter = require('./bikes')
+const scootersRouter = require('./scooters')
+const carsRouter = require('./cars')
+
+module.exports = app => {
+  app.use('/', indexRouter)
+  app.use('/passengers', passengersRouter)
+  app.use('/stations', stationsRouter)
+  app.use('/parking-units', parkingUnitsRouter)
+  app.use('/bikes', bikesRouter)
+  app.use('/scooters', scootersRouter)
+  app.use('/cars', carsRouter)
+}
