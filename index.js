edf8c290281758dcc1fcadac24ba60732704c9ba
tinas <ahmettinastepe0@gmail.com>
2021-01-23 22:04:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const indexRouter = require('./routes/index')
-const passengersRouter = require('./routes/passengers')
-const stationsRouter = require('./routes/stations')
-const bikesRouter = require('./routes/bikes')
-const scootersRouter = require('./routes/scooters')
+
+require('./mongo-connection')
 
 const app = express()
 app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
-app.use('/', indexRouter)
-app.use('/passengers', passengersRouter)
-app.use('/stations', stationsRouter)
-app.use('/bikes', bikesRouter)
-app.use('/scooters', scootersRouter)
+require('./routes/routers')(app)
 
 app.listen(3000, () => {
   console.log('app listening on 3000')
