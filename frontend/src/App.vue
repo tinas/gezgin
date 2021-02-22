0ea82bd472316429b99972b0548e7e7911a8cb91
tinas <ahmettinastepe0@gmail.com>
2021-02-22 14:48:31 +0300

    Implement reset.css and theme.css

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  #app
+    router-view
+</template>
+
+<style lang="scss">
+@import url('./assets/reset.css');
+@import url('./assets/theme.css');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,700;0,900;1,400&display=swap');
+
+html {
+  height: 100%;
+  overflow: auto;
+}
+
+body {
+  background: var(--bg-color);
+  height: 100%;
+  overflow: auto;
+}
+
+#app {
+  font-family: 'Nunito', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
