fcbaad5145bd16478f4e0f902d5fe95e4056d51e
tinas <ahmettinastepe0@gmail.com>
2021-02-22 15:11:06 +0300

    Implement VIconForCard component
    Used to create low opacity background icons on card components.

---
+++ frontend/src/components/VIconForCard.vue
@@ -0,0 +1,58 @@
+<script>
+import IconBike from '@/assets/icons/bike.svg'
+import IconCar from '@/assets/icons/car.svg'
+import IconScooter from '@/assets/icons/scooter.svg'
+
+export default {
+  name: 'VIconForCard',
+  components: {
+    IconBike,
+    IconCar,
+    IconScooter
+  },
+  props: {
+    iconName: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        return ['bike', 'scooter', 'car'].indexOf(value) !== -1
+      }
+    },
+    size: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+  .icon-wrapper
+    .icon
+      IconBike(v-if="iconName === 'bike'" :width="size" :height="size")
+      IconCar(v-else-if="iconName === 'car'" :width="size" :height="size")
+      IconScooter(v-else-if="iconName === 'scooter'" :width="size" :height="size")
+</template>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 999px;
+}
+
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  color: var(--primary-color);
+  background: var(--white-color);
+  border-radius: 999px;
+}
+</style>
