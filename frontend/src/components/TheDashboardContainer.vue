f271deb861a776d8f89e6757ec6c38122f128c57
tinas <ahmettinastepe0@gmail.com>
2021-02-22 15:07:03 +0300

    Implement dashboard container component

---
+++ frontend/src/components/TheDashboardContainer.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'TheDashboardContainer'
+}
+</script>
+
+<template lang="pug">
+  .dashboard-container
+    slot
+</template>
+
+<style lang="scss">
+body {
+  background: var(--dashboard-bg);
+}
+</style>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  margin: 20px;
+}
+</style>
