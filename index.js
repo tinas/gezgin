090d118641ff7e6d38f4851a94c6b8bf8486f3b3
tinas <ahmettinastepe0@gmail.com>
2021-02-27 21:18:00 +0300

    Implement `cors` package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('./mongo-connection')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 app.set('view engine', 'pug')
 
