e16fe2f27821fbaa889d66fc71013372b39aebbf
tinas <ahmettinastepe0@gmail.com>
2021-02-27 21:14:46 +0300

    The booking test should be rewritten according to the `parkingUnit`

--- tests/scenario/bookings.test.js
+++ tests/scenario/bookings.test.js
@@ -10,8 +10,8 @@ test('creates a new booking', async done => {
 
   const scooterToCreate = {
     code: '101122',
-    brand: 'Giant',
-    pricePerMinute: 1
+    brand: 'Razor E300',
+    pricePerMinute: 0.75
   }
 
   const originStationToCreate = {
@@ -22,22 +22,35 @@ test('creates a new booking', async done => {
   const passengerCreatedResponse = await request.post('/passengers').send(passengerToCreate).expect(200)
   const scooterCreatedResponse = await request.post('/scooters').send(scooterToCreate).expect(200)
   const originStationCreatedResponse = await request.post('/stations').send(originStationToCreate).expect(200)
+
+  const parkingUnitToCreate = {
+    code: '967736',
+    vehicleId: scooterCreatedResponse.body._id,
+    vehicleType: 'Scooter'
+  }
+
+  const parkingUnitCreatedResponse = await request
+    .post(`/stations/${originStationCreatedResponse.body._id}/parking-units`)
+    .send(parkingUnitToCreate)
+    .expect(200)
+
   const bookingResponse = await request
     .post(`/passengers/${passengerCreatedResponse.body._id}/current-booking`)
     .send({
-      originId: originStationCreatedResponse.body._id,
-      vehicleId: scooterCreatedResponse.body._id,
-      vehicleType: 'Scooter'
+      parkingUnitId: parkingUnitCreatedResponse.body._id
     })
     .expect(200)
 
   expect(bookingResponse.body).toMatchObject({
     passenger: passengerCreatedResponse.body,
-    origin: originStationCreatedResponse.body,
-    vehicle: scooterCreatedResponse.body,
-    vehicleType: 'Scooter',
     state: 0
   })
+  expect(bookingResponse.body.origin.parkingUnits).toHaveLength(1)
+  expect(bookingResponse.body.origin.parkingUnits).toEqual(
+    expect.arrayContaining([parkingUnitCreatedResponse.body._id])
+  )
+  expect(bookingResponse.body.parkingUnit.vehicle).toEqual(scooterCreatedResponse.body._id)
+  expect(bookingResponse.body.parkingUnit.vehicleType).toEqual('Scooter')
 
   done()
 })
@@ -51,8 +64,8 @@ test('ends the current booking', async done => {
 
   const carToCreate = {
     code: '101122',
-    brand: 'Giant',
-    pricePerMinute: 1
+    brand: 'Audi',
+    pricePerMinute: 1.25
   }
 
   const originStationToCreate = {
@@ -61,7 +74,7 @@ test('ends the current booking', async done => {
   }
 
   const destinationStationToCreate = {
-    name: 'Maltepe',
+    name: 'Tuzla',
     location: ['22.56', '65.78']
   }
 
@@ -70,10 +83,42 @@ test('ends the current booking', async done => {
   const originStationCreatedResponse = await request.post('/stations').send(originStationToCreate).expect(200)
   const destinationStationCreatedResponse = await request.post('/stations').send(destinationStationToCreate).expect(200)
 
+  const parkingUnitToCreateWithVehicle = {
+    code: '100230',
+    vehicleId: carCreatedResponse.body._id,
+    vehicleType: 'Car'
+  }
+
+  const parkingUnitToCreateWithoutVehicle = {
+    code: '100230',
+    vehicleType: 'Car'
+  }
+
+  const originParkingUnitCreatedResponse = await request
+    .post(`/stations/${originStationCreatedResponse.body._id}/parking-units`)
+    .send(parkingUnitToCreateWithVehicle)
+    .expect(200)
+
+  const destinationParkingUnitCreatedResponse = await request
+    .post(`/stations/${destinationStationCreatedResponse.body._id}/parking-units`)
+    .send(parkingUnitToCreateWithoutVehicle)
+    .expect(200)
+
   await request
     .post(`/passengers/${passengerCreatedResponse.body._id}/current-booking`)
     .send({
-      originId: originStationCreatedResponse.body._id,
+      parkingUnitId: originParkingUnitCreatedResponse.body._id
+    })
+    .expect(200)
+
+  await request
+    .patch(`/parking-units/${originParkingUnitCreatedResponse.body._id}`)
+    .send({vehicleType: 'Car'})
+    .expect(200)
+
+  await request
+    .patch(`/parking-units/${destinationParkingUnitCreatedResponse.body._id}`)
+    .send({
       vehicleId: carCreatedResponse.body._id,
       vehicleType: 'Car'
     })
@@ -84,14 +129,16 @@ test('ends the current booking', async done => {
     .send({destinationId: destinationStationCreatedResponse.body._id})
     .expect(200)
 
-  expect(finishBookingResponse.body).toMatchObject({
-    passenger: passengerCreatedResponse.body,
-    origin: originStationCreatedResponse.body,
-    destination: destinationStationCreatedResponse.body,
-    vehicle: carCreatedResponse.body,
-    vehicleType: 'Car',
-    state: 1
-  })
+  expect(finishBookingResponse.body.passenger).toEqual(passengerCreatedResponse.body._id)
+  expect(finishBookingResponse.body.state).toEqual(1)
+  expect(finishBookingResponse.body.origin.parkingUnits).toHaveLength(1)
+  expect(finishBookingResponse.body.origin.parkingUnits).toEqual(
+    expect.arrayContaining([originParkingUnitCreatedResponse.body._id])
+  )
+  expect(finishBookingResponse.body.destination.parkingUnits).toHaveLength(1)
+  expect(finishBookingResponse.body.destination.parkingUnits).toEqual(
+    expect.arrayContaining([expect.objectContaining({_id: destinationParkingUnitCreatedResponse.body._id})])
+  )
 
   done()
 })
