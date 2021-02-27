20ddf34b807490d3e2ec89dafe182a63a4a94b77
tinas <ahmettinastepe0@gmail.com>
2021-02-27 21:05:10 +0300

    Fix linter

--- models/parking-unit.js
+++ models/parking-unit.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose')
 
 const ParkingUnitSchema = new mongoose.Schema({
-  code: {type: String, required: true, maxlength: 6},
-  state: {type: Number, required: true},
-  station: {type: mongoose.Schema.Types.ObjectId, ref: 'Station'},
-  vehicle: {type: mongoose.Schema.Types.ObjectId, refPath: 'vehicleType', autopopulate: true},
-  vehicleType: {type: String, required: true, enum: ['Bike', 'Scooter', 'Car']}
+  code: {
+    type: String,
+    required: true,
+    maxlength: 6
+  },
+  state: {
+    type: Number,
+    required: true
+  },
+  station: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Station'
+  },
+  vehicle: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'vehicleType',
+    autopopulate: true
+  },
+  vehicleType: {
+    type: String,
+    required: true,
+    enum: ['Bike', 'Scooter', 'Car']
+  }
 })
 
 ParkingUnitSchema.plugin(require('mongoose-autopopulate'))
