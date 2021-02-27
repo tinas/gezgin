d047c344fb28fb4b09174ef539aa22736fb52860
tinas <ahmettinastepe0@gmail.com>
2021-02-27 21:03:47 +0300

    Remove the 'vehicle' reference
    Remove the 'vehicle' reference and replace it with
    the 'parkingUnit' reference

--- models/booking.js
+++ models/booking.js
@@ -1,14 +1,36 @@
 const mongoose = require('mongoose')
 
-const BookingSchema = new mongoose.Schema({
-  passenger: {type: mongoose.Schema.Types.ObjectId, ref: 'Passenger', autopopulate: {maxDepth: 1}},
-  origin: {type: mongoose.Schema.Types.ObjectId, ref: 'Station', autopopulate: {maxDepth: 1}},
-  destination: {type: mongoose.Schema.Types.ObjectId, ref: 'Station', autopopulate: {maxDepth: 1}},
-  vehicle: {type: mongoose.Schema.Types.ObjectId, refPath: 'vehicleType', autopopulate: true},
-  vehicleType: {type: String, required: true, enum: ['Bike', 'Scooter', 'Car']},
-  state: {type: Number, required: true}
-})
+const BookingSchema = new mongoose.Schema(
+  {
+    passenger: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Passenger'
+    },
+    origin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Station',
+      autopopulate: {maxDepth: 1}
+    },
+    destination: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Station',
+      autopopulate: {maxDepth: 1}
+    },
+    parkingUnit: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ParkingUnit',
+      autopopulate: {maxDepth: 1}
+    },
+    totalPrice: {
+      type: Number
+    },
+    state: {
+      type: Number,
+      required: true
+    }
+  },
   {timestamps: true}
+)
 
 BookingSchema.plugin(require('mongoose-autopopulate'))
 
