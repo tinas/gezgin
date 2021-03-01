ca2e200c8a16dd08c134074eb6c59cdcd15e3e46
tinas <ahmettinastepe0@gmail.com>
2021-03-01 15:19:11 +0300

    Implement `seedDatabase`

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const seedDatase = require('./lib/seedDatabase')
+
 mongoose.connect('mongodb://localhost/gezgin', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -7,6 +9,7 @@ mongoose.connect('mongodb://localhost/gezgin', {
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
+db.once('open', async () => {
   console.log('we are connected to mongodb!')
+  await seedDatase()
 })
