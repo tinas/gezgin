d4aedace6acdba355877cf0ca66d9a93f4e39c4d
tinas <ahmettinastepe0@gmail.com>
2021-03-04 12:23:47 +0300

    Auth redirects
    If there is no `passengerId`, the passenger must first log in to the site.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -13,27 +14,47 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
+    beforeEnter(to, from, next) {
+      if (store.state.passengerId) return next('/dashboard')
+      return next()
+    }
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('../views/Register.vue')
+    component: () => import('../views/Register.vue'),
+    beforeEnter(to, from, next) {
+      if (store.state.passengerId) return next('/dashboard')
+      return next()
+    }
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue')
+    component: () => import('../views/Dashboard.vue'),
+    beforeEnter(to, from, next) {
+      if (!store.state.passengerId) return next('/login')
+      return next()
+    }
   },
   {
     path: '/booking-start',
     name: 'BookingStart',
-    component: () => import('../views/BookingStart.vue')
+    component: () => import('../views/BookingStart.vue'),
+    beforeEnter(to, from, next) {
+      if (!store.state.passengerId) return next('/login')
+      return next()
+    }
   },
   {
     path: '/booking-end',
     name: 'BookingEnd',
-    component: () => import('../views/BookingEnd.vue')
+    component: () => import('../views/BookingEnd.vue'),
+    beforeEnter(to, from, next) {
+      if (!store.state.passengerId) return next('/login')
+      return next()
+    }
   }
 ]
 
